a8ce8c36c3f119c8cbffb99b0830cdad6e30bb43
ZifFiji <arthur.guerinault@epitech.eu>
2025-11-24 11:08:02 +0100

    fix: loginviewmodel warnings

--- client_mobile/AREA/AREA/Sources/Features/Login/ViewModels/LoginViewModel.swift
+++ client_mobile/AREA/AREA/Sources/Features/Login/ViewModels/LoginViewModel.swift
@@ -19,7 +19,7 @@ class LoginViewModel: ObservableObject {
 		}
 
 		func login() {
-				_ = LoginAction(
+			LoginAction(
 						parameters: LoginRequest(
 								username: username,
 								password: password
