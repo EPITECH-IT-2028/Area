210cd78e6e5ce8fda6de7dd8ef2190444688113d
Quentin L. <quentin.lapierre@epitech.eu>
2025-12-03 19:11:27 +0100

    fix(register): correct view rendering and remove layout
    - Removed the unused RegisterLayout component from the register route.
    - Fixed ContentView to use isMobile for responsive rendering, ensuring
    the
      correct view is displayed based on screen size.

--- apps/web/app/register/views/ContentView.tsx
+++ apps/web/app/register/views/ContentView.tsx
@@ -5,11 +5,11 @@ import DesktopRegisterView from "@/app/register/views/DesktopRegisterView";
 import MobileRegisterView from "@/app/register/views/MobileRegisterView";
 
 export default function ContentView() {
-  const isDesktop = useMediaQuery("(min-width: 767px)");
+  const isMobile = useMediaQuery("(max-width: 767px)");
 
   return (
     <main className="flex min-h-screen items-center justify-center bg-[url(/background.jpg)] bg-contain bg-top bg-no-repeat md:bg-cover md:bg-center">
-      {isDesktop ? <DesktopRegisterView /> : <MobileRegisterView />}
+      {isMobile ? <MobileRegisterView /> : <DesktopRegisterView />}
     </main>
   );
 }
