13d065cac3a5e673fce93b519aa387e56487a6cd
ZifFiji <arthur.guerinault@epitech.eu>
2025-12-08 10:59:01 +0100

    feat: log out button to settings

--- apps/mobile/AREA/AREA/Sources/Features/Settings/Views/SettingsView.swift
+++ apps/mobile/AREA/AREA/Sources/Features/Settings/Views/SettingsView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct SettingsView: View {
+	@EnvironmentObject var authState: AuthState
+	@State var wantToLogout: Bool = false
 	var body: some View {
-		NavigationStack {
-			List {
-				NavigationLink(destination: UpdateServerPathView()) {
-					Text(LocalizedStringResource.settingsUpdateServerPathTitle)
-						.foregroundStyle(Color.primary)
+		VStack {
+			NavigationStack {
+				List {
+					NavigationLink(destination: UpdateServerPathView()) {
+						Text(LocalizedStringResource.settingsUpdateServerPathTitle)
+							.foregroundStyle(Color.primary)
+					}
+					Button(
+						LocalizedStringResource.settingsLogout,
+						action: {
+							wantToLogout = true
+						}
+					)
+					.foregroundStyle(Color(.red))
+					.alert(
+						LocalizedStringResource.settingsWantToLogout,
+						isPresented: $wantToLogout
+					) {
+						Button(
+							LocalizedStringResource.settingsLogoutCancel,
+							role: .cancel
+						) {
+							wantToLogout = false
+						}
+						Button(
+							LocalizedStringResource.settingsLogoutConfirm,
+							role: .destructive
+						) {
+							do {
+								try authState.logout()
+							} catch let error {
+								print(error)
+							}
+						}
+					}
 				}
 			}
 		}
